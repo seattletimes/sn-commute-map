c94dc80d5d3584c368bb2aa6abc6c7e163ac0902
Audrey Carlsen <acarlsen@seattletimes.com>
2015-08-19 14:00:27 -0700

    roar stuff

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,36 @@
 require("component-responsive-frame/child");
 require("component-leaflet-map");
 
+var data = require("./censusData.geo.json");
+
 var mapElement = document.querySelector("leaflet-map");
 var L = mapElement.leaflet;
 var map = mapElement.map;
 
 map.scrollWheelZoom.disable();
+
+function getColor(d) {
+  return d > 0.09 ? '#006837' :
+         d > 0.06 ? '#1a9850' :
+         d > 0.03 ? '#66bd63' :
+         d > 0.01 ? '#a6d96a' :
+         d > 0 ?    '#d9ef8b' :
+         d > -.01 ? '#fee08b' :
+         d > -.03 ? '#fdae61' :
+         d > -.06 ? '#f46d43' :
+         d > -.09 ? '#d73027' :
+                    '#a50026' ;
+}
+
+function style(feature) {
+  return {
+    fillColor: getColor(feature.properties.jobschange),
+    weight: 0,
+    fillOpacity: 0.25
+  };
+}
+
+L.geoJson(data, {style: style}).addTo(map);
 
 var coordinates = {
   A: [47.45, -122.16],
@@ -24,12 +49,12 @@ var drawLine = function(location1, location2) {
   var pointList = [pointA, pointB];
 
   var polyline = new L.Polyline(pointList, {
-  color: '#4abe9d',
-  weight: 4,
-  opacity: 1,
-  smoothFactor: 1
-
+    color: '#4abe9d',
+    weight: 4,
+    opacity: 1,
+    smoothFactor: 1
   });
+
   polyline.addTo(map);
   return polyline;
 }
@@ -48,8 +73,6 @@ for (var location in coordinates) {
   }).addTo(map);
 };
 
-var current = 0;
-
 var onClick = function() {
   var current = document.querySelector(".current");
   var go;
@@ -61,7 +84,24 @@ var onClick = function() {
   if (!go) return;
   current.classList.remove("current");
   go.classList.add("current");
-  console.log(go.getAttribute("data-poi"));
+
+  var currentId = current.getAttribute("data-poi");
+
+  var currentStop = poi[currentId];
+  if (currentStop instanceof L.Marker) {
+    currentStop._icon.classList.remove("active");
+  } else {
+    currentStop.setStyle({color: "#4abe9d"});
+  }
+
+  var goId = go.getAttribute("data-poi");
+
+  var stop = poi[goId];
+  if (stop instanceof L.Marker) {
+    stop._icon.classList.add("active");
+  } else {
+    stop.setStyle({color: "#f57d20"});
+  }
 };
 
 var buttons = document.querySelectorAll(".controls .fa");
